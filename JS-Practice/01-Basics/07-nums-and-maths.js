b211739028cccbde642c2f78798561125c80139a
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-16 22:28:56 +0530

    modified

--- JS-Practice/01-Basics/07-nums-and-maths.js
+++ JS-Practice/01-Basics/07-nums-and-maths.js
@@ -1,5 +1,6 @@
 const score = 200;
 const balance = new Number(100); //explicity giving the number data type
+console.log("hello");
 console.log(score, balance);
 console.log(balance.toString());  //converts the number into string type
-console.log(balance.toFixed(3));  //giver the three zeros after decimal
+console.log(balance.toFixed(3));  //inserts the three zeros after decimal
