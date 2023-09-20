48a0cfaa0f8f6928db04ba88dd1ffd83b2b272d5
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-20 23:42:49 +0530

    new file

---
+++ JS-Practice/03-Basics/02-scopes.js
@@ -0,0 +1,12 @@
+var c =10; //globsl scope
+ 
+if (true){
+    let a = 10; //block scope and can be used only in this scope..outside it cant be accessed
+    const b = 20; //block scope and can be used only in this scope..outside it cant be accessed
+   // var c = 15;  it has a function/global scope and it is the reason scooping came into existence 
+    console.log("Inner :" + a, b, c);
+}
+let a = 13;
+const b = 25;
+var c // here c = 10 as declared above (by hoisting)
+console.log("outer :" + a, b, c);
