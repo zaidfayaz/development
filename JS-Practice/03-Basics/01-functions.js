6cc04acaa7fdfac718358c889ad3c0adadc94e1e
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-20 23:08:29 +0530

    modified

--- JS-Practice/03-Basics/01-functions.js
+++ JS-Practice/03-Basics/01-functions.js
@@ -33,3 +33,41 @@ function userLogin(username){
 console.log(userLogin()); 
 console.log(userLogin("Zaid"));
 
+
+function cartItemsPrice(...num){ //spread operator :we can pass n no. of arguments(value) without giving the n no. of parameters
+    return num
+
+}
+console.log(cartItemsPrice(200, 3000 , 40000 , ));
+
+//passing object in the function
+const user = {
+    name : "Zaid Fayaz",
+    course : "Web Development",
+    price : 4999
+
+}
+function handleObject(anyobject){
+    console.log(`The course ${anyobject.course} is teached by ${anyobject.name} @ ${anyobject.price} only`)
+
+}
+handleObject(user)  //pass the object name here
+//we can directly pass the object as well
+handleObject({
+    name : "Zaid Shawl",
+    course : "JavaScript",
+    price : 999
+
+}) 
+
+//passing array in the function
+const myArr = [200, 400, 500, 300];
+
+function arrayHandler(getArray){
+    return getArray[2]
+}
+console.log(arrayHandler(myArr));
+console.log(arrayHandler([200,100,400,600])); //we can also directly pass the array as well
+
+
+
