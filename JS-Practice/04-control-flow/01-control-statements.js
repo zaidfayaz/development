e57879c6979d59e22cd0e3cf34a5f792646a90d5
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-22 20:38:24 +0530

    modified

--- JS-Practice/04-control-flow/01-control-statements.js
+++ JS-Practice/04-control-flow/01-control-statements.js
@@ -12,9 +12,20 @@ const balance = 0;
 
 if ( balance >= 1000){
     console.log(`Account balance is maintained. Your balance is ${balance}`);
-}else if ( balance < 1000){
+}else if ( balance < 1000 && balance > 0){
     console.log(`Account balance is not maintained. Your balance is ${balance}`);
 }else{
- 
-    console.log(`Account balance is zero. Your balance is ${balance}`);
+  console.log(`Account balance is zero. Your balance is ${balance}`);
+}
+
+const userLoggedIn = true;
+const userDebitCard = true;
+const userLoggedInFromGoogle = false;
+const userLoggedInFromEmail = true;
+
+if (userLoggedIn && userDebitCard){
+    console.log("Allow user to do shopping");
+}
+if (userLoggedInFromEmail || userLoggedInFromGoogle){
+    console.log("Allow user to login");
 }
