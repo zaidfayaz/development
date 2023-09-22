fc2f847f287024676cc362f7d69d1cb0f9e6477f
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-22 20:08:28 +0530

    modified

--- JS-Practice/04-control-flow/01-control-statements.js
+++ JS-Practice/04-control-flow/01-control-statements.js
@@ -6,3 +6,15 @@ if (temperature <= 30) {
 else {
     console.log('Temperature is high');
 }
+
+//nested if else 
+const balance = 0;
+
+if ( balance >= 1000){
+    console.log(`Account balance is maintained. Your balance is ${balance}`);
+}else if ( balance < 1000){
+    console.log(`Account balance is not maintained. Your balance is ${balance}`);
+}else{
+ 
+    console.log(`Account balance is zero. Your balance is ${balance}`);
+}
