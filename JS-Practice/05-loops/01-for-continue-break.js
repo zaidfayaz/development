cc557bc824a5bad41b27664f956b358d1813896e
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-25 21:53:48 +0530

    modified

--- JS-Practice/05-loops/01-for-continue-break.js
+++ JS-Practice/05-loops/01-for-continue-break.js
@@ -33,3 +33,29 @@ for (let i = 1; i <=10; i++) {
     }
     
 }
+
+//loop on array
+let myArray = ["zaid", "faizan", "suhaib", "raqib"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
+// break and continue
+for (let i = 0; i <= 20; i++) {
+   if ( i == 5){
+        console.log("Detected 5");
+        break;
+    }
+    console.log(`The value is ${i}`);
+}
+
+for (let i = 0; i <= 20; i++) {
+    if ( i == 5){
+         console.log("Detected 5");
+         continue;
+     }
+     console.log(`The value is ${i}`);
+ }
+ 
