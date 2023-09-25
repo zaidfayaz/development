a3d412c30ec3df9c10d3813d462df47ff4ecc967
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-25 21:05:47 +0530

    modified

--- JS-Practice/05-loops/01-one.js
+++ JS-Practice/05-loops/01-one.js
@@ -13,3 +13,23 @@ for (let i = 0; i <= 10; i++) {
     console.log(element);
     
 }
+
+//loops inside loop
+for (let i = 0; i <=10; i++) {
+    console.log(`outer loop value: ${i}`);
+    for (let j = 0; j <=5; j++) {
+        console.log(`inner loop value: ${j} and outer loop value ${i}`);
+        
+    }
+    
+}
+
+// multiplication table
+for (let i = 1; i <=10; i++) {
+    console.log(`outer loop value: ${i}`);
+    for (let j = 1; j <=10; j++) {
+        console.log(i + "x" + j + "=" + i * j );
+        
+    }
+    
+}
