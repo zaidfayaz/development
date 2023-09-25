1cdab87f12843fa20c3b78519895ab616c9cdf43
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-25 20:52:33 +0530

    new file

---
+++ JS-Practice/05-loops/01-one.js
@@ -0,0 +1,15 @@
+//for loop
+ for (let index = 0; index <= 5; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        console.log("I love number 5");
+    }
+    console.log(element);
+    
+}
