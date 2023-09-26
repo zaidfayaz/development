530a1d9760daf5a71865bff8bb20a4c0f7330198
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-26 20:44:59 +0530

    modifed

--- JS-Practice/05-loops/02-while-and-do-while.js
+++ JS-Practice/05-loops/02-while-and-do-while.js
@@ -3,5 +3,14 @@ let index = 1
 while (index <= 10) {
     console.log(`The value of index is ${index}`);
     index ++;
-    
 }
+
+let myArray = ["zaid", "faizan", "suhaib", "raqib"];
+let arr = 0;
+
+while (arr < myArray.length) {
+    console.log(`The array is ${myArray[arr]}`);
+    arr ++;
+}
+
+//do-while loop
