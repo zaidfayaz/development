69553aaf07699d6fc74aaea8fafc195636992dc5
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-28 12:20:29 +0530

    modified

--- JS-Practice/05-loops/03-high-order-array-loops.js
+++ JS-Practice/05-loops/03-high-order-array-loops.js
@@ -1,8 +1,8 @@
 //for of loop
-const arr = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 for (const num of arr) {
     console.log(num);
-  }
+}
 
 const greetings = "Welcome Zaid";
 for (const greet of greetings) {
@@ -10,3 +10,16 @@ for (const greet of greetings) {
 }
 
 //maps 
+const map = new Map(); //The Map object holds key-value pairs and remembers the original insertion order of the keys. 
+map.set('SGR', "Srinagar");
+map.set('GD', "Ganderbal");
+map.set('BD', "Budgam");
+map.set('Sp', "Sopore");
+map.set('SGR', "Srinagar"); //its unique is maps collection so it wil ignore this duplicate key value pair
+
+console.log(map);
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+//for of loop doesnt work on objects
