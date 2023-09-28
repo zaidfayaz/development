f7548355a7aa37810a0ff5ecab50d5dc69d5fe07
zaidfayaz <zaidfayaz1@gmail.com>
2023-09-28 11:55:52 +0530

    new file

---
+++ JS-Practice/05-loops/03-high-order-array-loops.js
@@ -0,0 +1,12 @@
+//for of loop
+const arr = [1,2,3,4,5];
+for (const num of arr) {
+    console.log(num);
+  }
+
+const greetings = "Welcome Zaid";
+for (const greet of greetings) {
+    console.log(`The individual strings are : ${greet}`);
+}
+
+//maps 
