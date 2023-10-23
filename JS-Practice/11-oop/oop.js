886d411d6bf353698925e651adadf83951650fe1
zaidfayaz <zaidfayaz1@gmail.com>
2023-10-23 19:39:03 +0530

    modified

--- JS-Practice/11-oop/oop.js
+++ JS-Practice/11-oop/oop.js
@@ -1,10 +1,14 @@
-//constructor funtion
+//constructor funtion : its a reference to itself as well
 function user(username, loginCount, isLoggedIn) {
     this.username = username;
     this.loginCount = loginCount;
     this.isLoggedIn = isLoggedIn;
-    return this;
+    this.greetings =  function(){
+        console.log(`Welcome ${this.username}`);
+    }
+   // return this;
 }
-const userOne = new user("Zaid", 12, true) //new creates the multiplpe instances i.e constructor function
+const userOne = new user("Zaid", 12, true) //new creates empty object called instance i.e constructor function is called by new keyword
 const userTwo =  new user("Raqib", 10, false)
-console.log(userOne); 
+console.log(userOne.constructor); 
+console.log(userTwo);
