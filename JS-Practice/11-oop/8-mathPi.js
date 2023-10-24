fe5d6097a28cde4140949a29f97b94d735b9b7c6
zaidfayaz <zaidfayaz1@gmail.com>
2023-10-24 20:48:51 +0530

    new file

---
+++ JS-Practice/11-oop/8-mathPi.js
@@ -0,0 +1,21 @@
+const discriptor = Object.getOwnPropertyDescriptor(Math, 'PI') //tells about the hidden things of the object
+console.log(discriptor);
+
+const course = {
+    name : "JavaScript",
+    price : 3999,
+    isAvailable : true
+  
+}
+console.log(course);
+console.log(Object.getOwnPropertyDescriptor(course, "name"));
+
+Object.defineProperty(course, 'name', {
+    writable: false,
+    enumerable: false,
+    configurable: false
+})
+console.log(Object.getOwnPropertyDescriptor(course, "name"));
+
+course.name = "C"
+console.log(course);
