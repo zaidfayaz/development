e82c3fc2a730862d222d054ba5640c85d6d6d52b
zaidfayaz <zaidfayaz1@gmail.com>
2023-10-28 19:39:29 +0530

    new file

---
+++ problem-solving/02-day/4-square-root.js
@@ -0,0 +1,7 @@
+/**Write a js program to find the sqaure root the given number */
+const squareRoot = (num) => console.log(num * num);
+squareRoot(5)
+
+//using Math.sqrt
+const squareRoot2 = (num) => console.log(Math.sqrt(num));
+squareRoot(4)
