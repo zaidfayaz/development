b0d506ef793636da240a9525c5823db2817243dc
zaidfayaz <zaidfayaz1@gmail.com>
2023-10-28 19:25:13 +0530

    new file

---
+++ problem-solving/02-day/3-add-two-nos.js
@@ -0,0 +1,9 @@
+/*write a js program to add static  numbers*/
+let a = 10
+b = 20;
+console.log(`The sum of ${a} and ${b} is : ${a+b}`);
+
+/*write a js program to add dynamic  numbers*/
+const sum = (num1, num2) => console.log(`The sum of ${num1} and ${num2} is : ${num1 + num2}`);
+
+sum(20,30);
