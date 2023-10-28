c48c1e7352aa397c13ba4348b354348189a08149
zaidfayaz <zaidfayaz1@gmail.com>
2023-10-28 20:19:27 +0530

    new file

---
+++ problem-solving/03-day/2-swap-two-nos.js
@@ -0,0 +1,8 @@
+/*Write  a js program to swap two numbers*/ 
+const swapNumbers = (num1, num2,) =>{
+    let temp;
+    num1 = temp;  
+     num1 = num2;
+    num2 = temp
+}
+console.log(swapNumbers(4,5));
