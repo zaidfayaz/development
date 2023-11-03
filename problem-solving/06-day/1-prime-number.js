8744a256c29b3eb67a806950a057064716bfe3e3
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-03 21:24:42 +0530

    new file

---
+++ problem-solving/06-day/1-prime-number.js
@@ -0,0 +1,8 @@
+/*write a program to check whether the number is prime or not*/
+/*Prime number is positive integer that is only divisible by 1 and itself */
+const primeNumber = (number) =>{
+    if (number / 1 && number /number ){
+        console.log(`${number} is a prime number`);
+    }
+
+}
