a7caeb9d3576f1afc95c85ffb764e5f8354dfb69
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-04 22:24:45 +0530

    modified

--- problem-solving/06-day/2-largest-among-three.js
+++ problem-solving/06-day/2-largest-among-three.js
@@ -1,11 +1,12 @@
 /*write a program to find largest among three numbers */
 const largestNumber = (num1, num2, num3) =>{
-    if (num1 > num2 && num2 > num3){
+    if (num1 > num2 && num1 > num3){
         console.log(`${num1} is largest`);
     }else if ( num2 > num1 && num2 > num3){
         console.log(`${num2} is largest`);
     }else{
-        console.log(`${num3} is largest`););
+        console.log(`${num3} is largest`);
     }
 
 }
+largestNumber(62, 55, 65)
