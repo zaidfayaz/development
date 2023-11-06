675312435dcbfea0d8af09d20a1eb2e38b7a4e2d
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-06 21:46:26 +0530

    modified

--- problem-solving/06-day/3-factorial.js
+++ problem-solving/06-day/3-factorial.js
@@ -1,5 +1,12 @@
-function factorial(num){
-    
-
+let fact = 1
+function factorial(num) {
+    if (num == 0) {
+        console.log(`The factorial of ${num} is 1`);
+    } else {
+        for (let i = 1; i <= num; i++) {
+            fact = fact * i
+        }
+        console.log(`The factorial of ${num} is ${fact}`);
+    }
 }
-factorial(3)
+factorial(0)
