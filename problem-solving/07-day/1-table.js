18cfe9419807ead2438929b0545b92dd399df42d
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-08 21:43:55 +0530

    modified

--- problem-solving/07-day/1-table.js
+++ problem-solving/07-day/1-table.js
@@ -1,5 +1,7 @@
 const table=(num) =>{
-    for(let i = 1; i< 10 ; i++) {
-        return ``
+    for(let i = 1; i <= 10 ; i++) {
+        console.log(`${num} x ${i} = ${num * i}`)
     }
 }
+table(2);
+//table(6)
