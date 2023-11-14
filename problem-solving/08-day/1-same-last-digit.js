49c81f27d05bb7398bdaa62374d84a59f0f992e2
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-14 22:31:00 +0530

    modified

--- problem-solving/08-day/1-same-last-digit.js
+++ problem-solving/08-day/1-same-last-digit.js
@@ -1,6 +1,16 @@
 /*Write a program to check  if the number have the same last digit*/
 function checkLastDigit(num1, num2, num3){
-    console.log(num);
+    let res1 = num1 % 10;
+    let res2 = num2 % 10;
+    let res3 = num3 % 10;
 
+    if(res1 === res2 && res1 === res3)
+    {
+        console.log(`${num1}, ${num2}, ${num3} are having the same last digits`);
+    }else{
+        console.log(`${num1}, ${num2}, ${num3} are not having the same last digits`);
+    }
 }
-checkLastDigit(23, 43, 53)
+checkLastDigit(11, 21, 101)
+checkLastDigit(23, 45, 56)
+checkLastDigit(234332432, 657574532, 43356532)
