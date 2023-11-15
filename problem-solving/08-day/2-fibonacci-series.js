99bbccc934ba74a9f4fea375c7f5b6d1dd52155c
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-15 21:42:27 +0530

    modified

--- problem-solving/08-day/2-fibonacci-series.js
+++ problem-solving/08-day/2-fibonacci-series.js
@@ -1,8 +1,11 @@
 /*Write a program to print the fibonacci series */
 let num1 = 0;
 let num2 = 1;
-console.log(num1, num2);
+console.log(num1);
+console.log(num2);
 for (let i = 0; i <= 10; i++) {
-    result = num1 + num2;
-
+    let temp = num1 + num2  //0+1 = 1 =>temp
+    console.log(temp);
+    num1 = num2; //1: 1=1, 2: 1=1, 3: 2=2 so on.....
+    num2 = temp  // 1: 1=1, 2: 2=2, 3: 3=3 so on....
 }
