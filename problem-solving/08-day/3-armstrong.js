e434243a27cd570a658657cdb2742b4b807bae86
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-16 19:50:22 +0530

    new file

---
+++ problem-solving/08-day/3-armstrong.js
@@ -0,0 +1,14 @@
+/*Write to program to chech the armstrong number */
+function armstrongNumber(num){
+    let temp = num
+    let sum = 0;
+    while(temp > 0){
+        let digit = temp % 10
+        sum += digit **3
+        temp = parseInt(temp / 10)
+
+    }
+    if ()
+
+}
+armstrongNumber()
