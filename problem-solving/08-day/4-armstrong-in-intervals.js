f269ae824952ec292fc31be0d1c0f18e9954e080
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-17 20:17:15 +0530

    modified

--- problem-solving/08-day/4-armstrong-in-intervals.js
+++ problem-solving/08-day/4-armstrong-in-intervals.js
@@ -1,4 +1,19 @@
 /*Write to program to chech the armstrong numbers between an interval */
-function armstrong(){
-    
+function armstrong(num1, num2){
+    for (let i = 0; i<= num2; i++){
+        //console.log(i);
+        let temp = i;
+        let noOfDigits = i.toString().length;
+        let sum = 0;
+        while(temp > 0){
+            let digit = temp % 10;
+            temp = parseInt(temp/10);
+            sum += digit ** noOfDigits;
+        }
+        if(sum == i){
+            console.log(i);
+        }
+    }
+
 }
+armstrong(1, 1000)
