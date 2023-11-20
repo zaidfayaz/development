12b9cd37b9850821321605160e4edf61edb3b700
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-20 21:13:01 +0530

    modified

--- problem-solving/09-day/1-factors-of-numbers.js
+++ problem-solving/09-day/1-factors-of-numbers.js
@@ -1,7 +1,12 @@
 /*write a program to find the factor of a number */
-const factorNumber(num)=>{
+const factorNumber = (num)=>{
+    console.log(`The factors of ${num} are:`);
     for (let i = 0; i <= num; i++){
-        
+        //console.log(i);
+        if(num%i == 0){
+          
+            console.log(`${i}`);
+        }
     }
-
 }
+factorNumber(12)
