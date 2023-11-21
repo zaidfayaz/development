53f942ca9b8752ece22a4014736798e0a3898e3a
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-21 20:35:56 +0530

    new file

--- problem-solving/09-day/1-factors-of-numbers.js
+++ problem-solving/09-day/1-factors-of-numbers.js
@@ -1,4 +1,4 @@
-/*write a program to find the factor of a number */
+/*write a program to find the factors of a number */
 const factorNumber = (num)=>{
     console.log(`The factors of ${num} are:`);
     for (let i = 0; i <= num; i++){
---
+++ problem-solving/09-day/2-sum-of-natural-numbers.js
@@ -0,0 +1,1 @@
+/*Write a program to find the  sum of natural numbers using recursion */
