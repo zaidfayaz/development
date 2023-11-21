6fd8f00937281b064e57765eb950551afe2f6434
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-21 20:47:12 +0530

    modified

--- problem-solving/09-day/2-sum-of-natural-numbers.js
+++ problem-solving/09-day/2-sum-of-natural-numbers.js
@@ -1,1 +1,10 @@
 /*Write a program to find the  sum of natural numbers using recursion */
+function naturalSum(num){
+    if(num > 0){
+        return num + naturalSum(num - 1)
+        //output 5 + 4 + 3 + 2 + 1 + 0 = 15
+    }else{
+        return num;
+    }
+}
+console.log(naturalSum(5));
