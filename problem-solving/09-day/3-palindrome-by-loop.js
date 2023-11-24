fc76dd8b9fc90c86c990b035d7ee93dca3cc1195
zaidfayaz <zaidfayaz1@gmail.com>
2023-11-24 20:02:54 +0530

    modified

--- problem-solving/09-day/3-palindrome-by-loop.js
+++ problem-solving/09-day/3-palindrome-by-loop.js
@@ -1,13 +1,13 @@
 /*Write a program to check the palindrome using Loop */
 const palindrome = (str)=>{
     let len = str.length
-    let msg = "t as a palindrome"
+    let msg = "It is a Palindrome"
     for(let i = 0; i < len/2; i++){
         if(str[i] != str[len - 1 - i]){
-            console.log("number is palindrome");
+            msg = 'It is not a Palindrome'
         }
     }
     console.log(`${str} : ${msg}`);
 
 }
-palindrome("madam")
+palindrome("racecar")
